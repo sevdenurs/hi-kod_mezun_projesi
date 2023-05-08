363664ccb899268d220e1a96229189e0a384659a
Sevde Nur TAŞKIN <119888860+sevdenurs@users.noreply.github.com>
2023-05-08 23:47:18 +0300

    Add files via upload

---
+++ oyunkodları/PlayerAttack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerAttack : MonoBehaviour
+
+{
+    [SerializeField] GameObject attackArea = default;
+    private Animator _animator;
+    private bool attacking = false;
+
+    private float timeToAttack = 0.25f;
+    private float timer = 0f;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+
+        if (attacking)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= timeToAttack)
+            {
+                timer = 0;
+                _animator.SetBool("isAttacking", false);
+                attacking = false;
+                attackArea.SetActive(attacking);
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        _animator.SetBool("isAttacking", true);
+        attacking = true;
+        attackArea.SetActive(attacking);
+        Debug.Log("Saldırdı");
+    }
+}
